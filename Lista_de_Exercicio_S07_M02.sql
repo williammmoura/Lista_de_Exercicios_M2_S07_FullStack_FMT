40f47527bfaa91c7e7fb25c9e2ea56e919f76989
William Monte de Moura <williamonte@gmail.com>
2023-06-21 15:49:03 -0300

    Exercício 03

--- Lista_de_Exercicio_S07_M02.sql
+++ Lista_de_Exercicio_S07_M02.sql
@@ -10,17 +10,6 @@ create table Usuario(
 	rg integer
 );
 
---  Criando a tabela "Jogo";
-create table jogo(
-	id integer primary key,
-	nome varchar (50),
-	data_lancamento date,
-	plataforma varchar (25),
-	genero varchar (15);
-	FOREIGN KEY (plataforma_id) REFERENCES Plataforma(id),
-  	FOREIGN KEY (genero_id) REFERENCES Genero(id)
-);
-
 --  Criando a tabela "Genero";
 create table Genero(
 	id integer primary key,
@@ -31,6 +20,26 @@ create table Genero(
 create table Plataforma(
 	id integer primary key,
 	nome varchar (50)
+);
+
+--  Criando a tabela "Jogo";
+create table jogo(
+	id integer primary key,
+	nome varchar (50),
+	data_lancamento date,
+	plataforma_id integer,
+	genero_id integer,
+	FOREIGN KEY (plataforma_id) REFERENCES Plataforma(id),
+  	FOREIGN KEY (genero_id) REFERENCES Genero(id)
+);
+
+-- Criação da tabela "Usuario_Jogo";
+create table Usuario_Jogo(
+  usuario_id INT,
+  jogo_id INT,
+  FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
+  FOREIGN KEY (jogo_id) REFERENCES Jogo(id),
+  PRIMARY KEY (usuario_id, jogo_id)
 );
 
 --  Criando a tabela "Foto";
@@ -45,6 +54,37 @@ create table Foto(
 create table Video(
 	id integer primary key,
 	jogo_id integer,
-	FOREIGN KEY (jogo_id) REFERENCES Jogo(id)
+	FOREIGN KEY (jogo_id) REFERENCES Jogo(id),
 	url varchar (500)
 );
+
+-- Inserir a plataforma "Playstation 4" na tabela Plataforma
+INSERT INTO Plataforma (id, nome)
+VALUES (1, 'Playstation 4');
+
+-- Inserir a plataforma "Playstation 5" na tabela Plataforma
+INSERT INTO Plataforma (id, nome)
+VALUES (2, 'Playstation 5');
+
+-- Inserir a plataforma "Desktop" na tabela Plataforma
+INSERT INTO Plataforma (id, nome)
+VALUES (3, 'Desktop');
+
+-- Inserir o gênero "Simulação da Vida Real" na tabela Genero
+INSERT INTO Genero (id, nome)
+VALUES (1, 'Simulação da Vida Real');
+
+-- Adicionar o jogo "The Sims 4 Base" à tabela Jogo
+INSERT INTO Jogo (id, nome, data_lancamento, plataforma_id, genero_id)
+VALUES (1, 'The Sims 4 Base', '2023-01-01', 
+        (SELECT id FROM Plataforma WHERE nome = 'Playstation 4'),
+        (SELECT id FROM Genero WHERE nome = 'Simulação da Vida Real'));
+
+-- Criar o usuário "Derpson da Silva"
+INSERT INTO Usuario (id, nome, login, senha, email, dt_nascimento, cpf, rg)
+VALUES (1, 'Derpson da Silva', 'derpinho', 'derpinho91', 'derpinho91@hotmail.com', '1991-01-01', '123.123.123-12', '4.123.123');
+
+-- Adicionar o jogo "The Sims 4 Base" à biblioteca do usuário "Derpson da Silva"
+INSERT INTO Usuario_Jogo (usuario_id, jogo_id)
+VALUES (1, 1);
+
